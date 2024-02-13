7766aa1dc8997a3ebf2c6e617b25f20d2d64e522
Keamber Pearson <kbrianna23@gmail.com>
2024-02-12 20:08:57 -0500

    commit please

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCdJeMze2qKwMCMz-w8dKKMIKAhIlyC9k8",
+    authDomain: "clone-46535.firebaseapp.com",
+    projectId: "clone-46535",
+    storageBucket: "clone-46535.appspot.com",
+    messagingSenderId: "70675072412",
+    appId: "1:70675072412:web:2d13f289eb40f13f0cb459",
+    measurementId: "G-CJVQCBTP62"
+  };
